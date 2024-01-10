e9717a246398fbd61bc353e5a1952f82275d9d37
kiko22223 <63966405+kiko22223@users.noreply.github.com>
2024-01-10 18:42:03 +0100

    Makeing Smart adding feature test, trying to simplify adding food to list

---
+++ ShoppingList/HoldForMoreView.swift
@@ -0,0 +1,18 @@
+//
+//  HoldForMoreView.swift
+//  ShoppingList
+//
+//  Created by Christian Marušák on 10/01/2024.
+//
+
+import SwiftUI
+
+struct HoldForMoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HoldForMoreView()
+}
