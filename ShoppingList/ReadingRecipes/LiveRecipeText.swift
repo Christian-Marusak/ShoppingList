150435fa13d5d20450b78edebaf385e29efe64c7
kiko22223 <63966405+kiko22223@users.noreply.github.com>
2024-03-19 22:14:49 +0100

    Makeing view that will read recipes from photo

--- ShoppingList/ReadingRecipes/LiveRecipeText.swift
+++ ShoppingList/ReadingRecipes/LiveRecipeText.swift
@@ -6,16 +6,72 @@
 //
 
 import SwiftUI
-import VisionKit
+import Vision
 struct LiveRecipeText: View {
+    
+    @State var recognizedText = ""
+    @State private var showCamera = false
+    @State private var selectedImage: UIImage?
+    @State var image: UIImage?
+    
+    
+    
+    func ocr() {
+        let image = UIImage(named: "quote")
+        
+        if let cgImage = image?.cgImage {
+            
+            // Request handler
+            let handler = VNImageRequestHandler(cgImage: cgImage)
+            
+            let recognizeRequest = VNRecognizeTextRequest { (request, error) in
+                
+                // Parse the results as text
+                guard let result = request.results as? [VNRecognizedTextObservation] else {
+                    return
+                }
+                
+                // Extract the data
+                let stringArray = result.compactMap { result in
+                    result.topCandidates(1).first?.string
+                }
+                
+                // Update the UI
+                DispatchQueue.main.async {
+                    recognizedText = stringArray.joined(separator: "\n")
+                }
+            }
+            
+            // Process the request
+            recognizeRequest.recognitionLevel = .accurate
+            do {
+                try handler.perform([recognizeRequest])
+            } catch {
+                print(error)
+            }
+            
+        }
+    }
+    
     var body: some View {
         Button("Start scanning") {
+            self.showCamera.toggle()
             print("Started scanning")
-        }.padding()
+        }
+        .fullScreenCover(isPresented: self.$showCamera) {
+            accessCameraView(selectedImage: self.$selectedImage)
+        
+        .padding()
         .foregroundColor(.white)
         .background(Capsule().fill(Color.blue))
     }
 }
+
+
+
+
+
+
 
 #Preview {
     LiveRecipeText()
