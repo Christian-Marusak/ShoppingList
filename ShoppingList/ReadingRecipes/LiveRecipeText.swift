92fced9c77f4f941981c63498530a426859c8cc4
kiko22223 <63966405+kiko22223@users.noreply.github.com>
2024-03-27 19:51:51 +0100

    Finalized reading and choosing from photo library or camera

--- ShoppingList/ReadingRecipes/LiveRecipeText.swift
+++ ShoppingList/ReadingRecipes/LiveRecipeText.swift
@@ -8,144 +8,76 @@
 import SwiftUI
 import Vision
 import PhotosUI
-//struct LiveRecipeText: View {
-//    @State var showCamera = false
-//    @State var recognizedText = ""
-//    @State var selectedImage : UIImage?
-//    
-//    func ocr(image : UIImage) {
-//        
-//        if let cgImage = image.cgImage {
-//            
-//            // Request handler
-//            let handler = VNImageRequestHandler(cgImage: cgImage)
-//            
-//            let recognizeRequest = VNRecognizeTextRequest { (request, error) in
-//                
-//                // Parse the results as text
-//                guard let result = request.results as? [VNRecognizedTextObservation] else {
-//                    return
-//                }
-//                
-//                // Extract the data
-//                let stringArray = result.compactMap { result in
-//                    result.topCandidates(1).first?.string
-//                }
-//                
-//                // Update the UI
-//                DispatchQueue.main.async {
-//                    
-//                    recognizedText = stringArray.joined(separator: "\n")
-//                    print(recognizedText)
-//                    print("Recognizing text")
-//                }
-//            }
-//            
-//            // Process the request
-//            recognizeRequest.recognitionLevel = .accurate
-//            do {
-//                try handler.perform([recognizeRequest])
-//            } catch {
-//                print(error)
-//            }
-//            
-//        }
-//    }
-//    
-//    // Coordinator will help to preview the selected image in the View.
-//    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-//        var picker: accessCameraView
-//        
-//        init(picker: accessCameraView) {
-//            self.picker = picker
-//        }
-//        
-//        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-//            guard let selectedImage = info[.originalImage] as? UIImage else { return }
-//            self.picker.selectedImage = selectedImage
-//            self.picker.isPresented.wrappedValue.dismiss()
-//        }
-//    }
-//
-//    
-//    struct accessCameraView: UIViewControllerRepresentable {
-//        
-//        @Binding var selectedImage: UIImage?
-//        @Environment(\.presentationMode) var isPresented
-//        
-//        func makeUIViewController(context: Context) -> UIImagePickerController {
-//            let imagePicker = UIImagePickerController()
-//            imagePicker.sourceType = .camera
-//            imagePicker.allowsEditing = true
-//            imagePicker.delegate = context.coordinator
-//            return imagePicker
-//        }
-//        
-//        func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
-//            
-//        }
-//
-//        func makeCoordinator() -> Coordinator {
-//            return Coordinator(picker: self)
-//        }
-//    }
-//    
-//    var body: some View {
-//        Button("Start scanning") {
-//            self.showCamera.toggle()
-//        }
-//            .padding()
-//            .foregroundColor(.white)
-//            .background(Capsule().fill(Color.blue))
-//            .fullScreenCover(isPresented: $showCamera) {
-//                accessCameraView(selectedImage: self.$selectedImage)
-//            }.onDisappear(perform: {
-//                if let selectedSafeImage = selectedImage {
-//                    ocr(image: selectedSafeImage)
-//                }
-//            })
-//    }
-//}
 
 struct LiveRecipeText: View {
     
     @State var recognizedText = ""
     @State var imagePicker: PhotosPickerItem?
-    @State var selectedImage: UIImage?
+    @State var selectedImage: UIImage = .iconLogo
+    @State private var showPhotoOptions = false
+    @State private var photoSource: PhotoSource?
+    
+    enum PhotoSource: Identifiable {
+        case photoLibrary
+        case camera
+        
+        var id: Int {
+            hashValue
+        }
+    }
     
     var body: some View {
         VStack {
-            Text("OCR using Vission")
-                .font(.title)
-            
-            PhotosPicker(selection: $imagePicker, matching: .images) {
-                Text("select image")
-                Image(uiImage: selectedImage ?? UIImage(resource: .iconLogo))
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 150)
-            }.onChange(of: imagePicker) { _, _ in
-                Task{
-                    if let imagePicker,
-                       let data = try? await imagePicker.loadTransferable(type: Data.self) {
-                        if let image = UIImage(data: data){
-                            selectedImage = image
-                        }
+            Spacer()
+            Button(action: {showPhotoOptions.toggle()}, label: {
+                Capsule()
+                    .overlay {
+                        Text("Select picture to scan")
+                            .foregroundStyle(.white)
                     }
-                }
-            }
+                    .frame(width: 200,height: 40)
+                
+            })
             
-           
             
-            Button("Recognize Text"){
-                ocr(inImage: selectedImage!)
-            }
-            
+            Button(action: {
+                ocr(inImage: selectedImage)
+            }, label: {
+                Capsule()
+                    .overlay {
+                        Text("scan image")
+                            .foregroundStyle(.white)
+                    }
+                    .frame(width: 200,height: 40)
+                    .foregroundStyle(.red)
+                
+            })
+            Image(uiImage: selectedImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 200)
             TextEditor(text: $recognizedText)
+        }.fullScreenCover(item: $photoSource) { source in
+            switch source {
+            case .photoLibrary: ImagePicker(sourceType: .photoLibrary, selectedImage: $selectedImage).ignoresSafeArea()
+            case .camera: ImagePicker(sourceType: .camera, selectedImage: $selectedImage).ignoresSafeArea()
+            }
         }
+        
+        .actionSheet(isPresented: $showPhotoOptions, content: {
+            ActionSheet(title: Text("Choose camera or photo library"), buttons: [
+                .default(Text("Camera"), action: {
+                    self.photoSource = .camera
+                }),
+                .default(Text("Photo library"), action: {
+                    self.photoSource = .photoLibrary
+                })
+            ])
+        })
         .padding()
         
     }
+    
     
     func ocr(inImage: UIImage) {
         
